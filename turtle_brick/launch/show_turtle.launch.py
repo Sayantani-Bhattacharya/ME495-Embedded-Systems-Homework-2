410cf88b5dad2db54f0514e8239998a753a2db29
sayantani <sayantanibhattacharya2025@u.northwestern.edu>
2024-10-24 19:34:04 -0500

    Naming conventions.

---
+++ turtle_brick/launch/show_turtle.launch.py
@@ -0,0 +1,38 @@
+"""Display the slidebot in rviz."""
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import Command, EqualsSubstitution, IfElseSubstitution, LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import ExecutableInPackage, FindPackageShare
+
+def generate_launch_description():
+    return LaunchDescription([
+        DeclareLaunchArgument("robot", default_value='turtle', description="The xacro file to load, can be either 'slide' or 'chained'"),
+        Node(
+            package="robot_state_publisher",
+            executable="robot_state_publisher",
+            parameters=[
+                {"robot_description" :
+                 Command([ExecutableInPackage("xacro", "xacro"), " ",
+                          PathJoinSubstitution(
+                              #A plain urdf file is also a valid xacro file so we use xacro here for convenience only
+                              [FindPackageShare("turtle_brick"),
+                               IfElseSubstitution(
+                                   EqualsSubstitution(LaunchConfiguration("robot"), "slide"),
+                                   if_value="turtle.urdf",
+                                #    else_value="chained.urdf.xacro"
+                                   )
+                               ])])}
+            ]
+        ),
+        Node(
+            package="joint_state_publisher_gui",
+            executable="joint_state_publisher_gui"
+        ),
+        Node(
+            package="rviz2",
+            executable="rviz2",
+            arguments=["-d", PathJoinSubstitution([FindPackageShare("me495_urdf"), "rviz_config.rviz"])]
+        )
+        ])
